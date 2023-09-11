37cac21fadf9f4b483476d70d5511cc7649676ba
MarcusL00 <marcuswind123@gmail.com>
2023-09-11 20:28:34 +0200

    updated

---
+++ Trying again/Trying again/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Trying_again
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperHero superHero1 = new SuperHero()
+            {
+                // Filling in data into the objects
+                Name = "Clark kent",
+                Age = 35,
+                Alias = "Superman",
+                SuperPowers = "Superhuman strength, speed, and flight",
+                ArchNemesis = "Lex Luthor",
+                Motivation = "To protect Earth and its people from threats"
+
+
+            };
+
+            superHero1.Introduction();
+        }
+
+    }
+}
---
+++ Trying again/Trying again/SuperHero.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trying_again
+{
+    public class SuperHero
+    {
+        private string _name = "";
+        private int? _age;
+        private string? _alias = "";
+        private string? _superPowers = "";
+        private string? _archNemesis = "";
+        private string? _motivation = "";
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int? Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string? Alias
+        {
+            get { return _alias; }
+            set { _alias = value; }
+        }
+
+        public string? SuperPowers
+        {
+            get { return _superPowers; }
+            set { _superPowers = value; }
+        }
+        public string? ArchNemesis
+        {
+            get { return _archNemesis; }
+            set { _archNemesis = value; }
+        }
+        public string? Motivation
+        {
+            get { return _motivation; }
+            set { _motivation = value; }
+        }
+        public void Introduction()
+        {
+            Console.WriteLine($"I am {Name}, im {Age} years old, and also known as {Alias}. i have special ability such as {SuperPowers}. My arch Nemisis is {ArchNemesis}");
+        }
+    }
+}
